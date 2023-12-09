34040b4cea55c025b8d201758962f8053658d17a
Yurii <yurii.rudenko26@gmail.com>
2023-12-09 17:20:31 +0200

    added-sets

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,24 +5,42 @@ import Card from '../Cards/Card/Card';
 import './Main.css';
 import { shuffleArray } from '../../Functions/shuffleArray';
 import AutoCarousel from './AutoCarousel';
+import { checkBadSets } from '../../Functions/checkBadSets';
 
 const Main = (props) => {
 
     const { course } = props
 
     const [recommendedCards, setRecommendedCards] = useState([])
+    const [sets, setSets] = useState([])
     const [width, setWidth] = useState(window.innerWidth)
     const [show, setShow] = useState(4)
 
     useEffect(() => {
+        console.log(sets)
+    }, [sets])
 
-        const random = Math.floor(Math.random() * 10) + 2
+    useEffect(() => {
+
+        const random = Math.floor(Math.random() * 20) + 2
 
         axios.get(`https://api.scryfall.com/cards/search?page=${random}&order=edhrec&dir=asc&q=-is%3Afunny+-is:digital+-t:land`)
 
             .then(response => setRecommendedCards(shuffleArray(response.data.data)))
             .catch(() => setRecommendedCards([]))
 
+        axios.get('https://api.scryfall.com/sets')
+            .then(response => {
+                const newSets = response.data.data.filter(set => checkBadSets(set)).map(set => ({
+                    name: set.name,
+                    card_count: set.card_count,
+                    image: set.icon_svg_uri,
+                    id: set.id,
+                    code: set.code,
+                }));
+
+                width > 880 ? setSets(newSets.slice(0, 32)) : setSets(newSets.slice(0, 16))
+            })
     },  [])
 
     const handleResize = () => {
@@ -68,6 +86,19 @@ const Main = (props) => {
 
             <div className="latest-sets">
                 <p className="latest-sets header">Latest Sets</p>
+                <div className="latest-sets-container">
+                    {sets.length > 0 && 
+                        sets.map(set => {
+                            return (
+                                <div key={set.name} className="set-container">
+                                    <img src={set.image} alt={set.name}></img>
+                                    <p>{set.name}</p>
+                                </div>
+                            )
+
+                        })
+                    }
+                </div>
             </div>
 
             <p className='recommended-cards header'>Recommended cards</p>
